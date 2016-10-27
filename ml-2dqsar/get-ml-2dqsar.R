d03860ceac02c19453e5f52a4c517e4564de599c
zusongpeng <zusongpeng@gmail.com>
2016-10-27 13:15:56 +0800

    add get parameters from ml-2dqsar.

---
+++ ml-2dqsar/get-ml-2dqsar.R
@@ -0,0 +1,164 @@
+### run ml-2dqsar
+### szu
+### 2016-10-20
+
+### accept args
+
+args <- commandArgs(TRUE)
+### set parameter.
+jobdir <- "/data/home/szu/Lab/ml-2dqsar/code/result/"
+datadir <- "/data/home/szu/Lab/ml-2dqsar/"
+ml2dqsar_dir <- "/data/home/szu/Lab/ml-2dqsar/code/"
+
+ml2dqsar_filenm <- "FuncPreComPiHierBayes.R"
+wiperfunc_filenm <- "wiperdata.R"
+
+## gpcr_names <- c("CPIs_GPCR_monoamine_receptor",
+##                 "CPIs_GPCR_nucleotide-like_receptor",
+##                 "CPIs_GPCR  lipid-like ligand receptor",
+##                 "CPIs_Peptide GPCR")
+gpcr_names <- c("lipidlike",
+                "monoamine",
+                "nucleotidelike",
+                "peptide",
+                "others")
+kinase_names <- c("kinase_agc", "kinase_camk","kinase_ck1","kinase_cmgc",
+                  "kinase_ste","kinase_tk","kinase_tkl")
+
+fealist <- c("macc","phychem","fingerprint","morgan")
+isbinaryf <- c(TRUE, FALSE, TRUE, TRUE)
+
+nfold <- 5
+lower_limit <- 50
+upper_limit <- 1000
+sigma2dot <- 100
+iternum <- 1000
+
+isGPCR <- TRUE
+subd_index= c(1) # subdirs choose index
+fc_index= c(1) # feature choose index
+
+cutoff = 0.05 # to filter the features.
+###------keep unchanged below----------###
+### packages
+library(methods)
+library(glmnet)
+library(data.table)
+###library(ggplot2)
+library(optimx)
+###library(doParallel)
+###library(foreach)
+###library(caret)
+library(matrixStats)
+library(Metrics)
+
+### set dir, and load func.
+setwd(jobdir)
+source(paste(ml2dqsar_dir,ml2dqsar_filenm,sep=""))
+source(paste(ml2dqsar_dir,wiperfunc_filenm,sep=""))
+
+### load features.
+features <- fealist[fc_index]
+isbinaryfc <- isbinaryf[fc_index]
+if(isGPCR){
+  subdirs <- gpcr_names[subd_index]
+} else{
+  subdirs <- kinase_names[subd_index]
+}
+
+### run
+for(f in 1:length(features)){# f for feature
+  for(k in 1:length(subdirs)){ # k for subdirs.
+    ## load data.
+    ldat = loadfiles(datadir,gpcr_nm, kinase_nm,isGPCR,f,k)
+    files = ldat$allfiles
+    dirs = ldat$protnms
+    oneGroup = list()
+    len = list()
+    oneGroup_xy = list()
+    for (i in (1:length(files))){
+      oneGroup[[i]] <- fread(as.character(files[i][[1]]), sep=',',header=TRUE)
+      len[i] <- length(oneGroup[[i]]$aveaffinty)
+      ## load data, take -log10 for y.
+      oneGroup[[i]]$aveaffinty <- -log10(oneGroup[[i]]$aveaffinty)
+      oneGroup_xy$X[[i]] <- subset(oneGroup[[i]], select = c(-V1,-aveaffinty))
+      oneGroup_xy$y[[i]] <- subset(oneGroup[[i]], select = aveaffinty)
+    }
+    keepft <- bifeakeep(oneGroup_xy,cutoff)
+    ##for(i in length(oneGroup_xy$y)){
+    ## with = FALSE is needed since it's data.table.  f*ck!!!
+    ## But it cannot change itself. f*ck!!!
+    ##  oneGroup_xy$X[[i]] = oneGroup_xy$X[[i]][ ,keepft, with=FALSE]
+    ##}
+    ## scale data
+    for (i in (1:length(files))){
+      oneGroup_xy$X[[i]] <- scale(oneGroup_xy$X[[i]], center=TRUE, scale = FALSE)
+      oneGroup_xy$y[[i]] <- scale(oneGroup_xy$y[[i]], center=TRUE, scale = FALSE)
+    }
+    ## filter based on compounds' number.
+    cpi <- list()
+    cpi$proteins <- list()
+    cpi$rootMSE  <- c()
+    j = 0
+    for(i in 1:length(oneGroup_xy$y)){
+      if(len[[i]]>=lower_limit & len[[i]] <= upper_limit){
+        j = j + 1
+        ## as.character is needed since dirs is factor. f*ck!!!
+        cpi$proteins[j] = as.character(dirs[[i]])
+        cpi$y[[j]] <- oneGroup_xy$y[[i]] # log of affinity
+        ## after scale, not data.table. f*ck!!!
+        cpi$X[[j]] <- oneGroup_xy$X[[i]][ ,keepft] # Matrix of the predictors.
+      }
+    }
+    ## check j == 0 ; continue
+    ## if(j==0){next;}
+    ## filter features, and update cpi
+    cpi$omega_matrix = matrix(nrow=length(cpi$y),ncol=ncol(cpi$X[[1]]))
+    for(i in 1:length(cpi$y)){
+      ## label data for n-fold cross validation.
+      ## flds <- createFolds(cpi$y[[i]],
+      ##                     k = nfold,
+      ##                     list=TRUE,
+      ##                     returnTrain = FALSE)
+      flds <- cut(seq(1,length(cpi$y[[i]])),breaks=nfold,labels=FALSE)
+      tmp_mse = 0
+      for(j in 1:nfold){
+        testindex <- which(flds==j,arr.ind=TRUE)
+        fit <- glmnet(x = cpi$X[[i]][-testindex,],
+                      y = cpi$y[[i]][-testindex],
+                      lambda=cv.glmnet(cpi$X[[i]][-testindex,],
+                                       y = cpi$y[[i]][-testindex],
+                                       intercept = FALSE, nfold = nfold)$lambda.min,
+                      intercept = FALSE)
+        test_result <- predict(fit,
+                               cpi$X[[i]][testindex,])
+        tmp_mse = tmp_mse + sqrt(mse(cpi$y[[i]][testindex],test_result))
+      } # end of nfold for j.
+      cpi$rootMSE[[i]] <- tmp_mse /nfold
+      fit <- glmnet(x = cpi$X[[i]],
+                    y = cpi$y[[i]],
+                    lambda=cv.glmnet(cpi$X[[i]],y = cpi$y[[i]],intercept = FALSE, nfold = nfold)$lambda.min,
+                    intercept = FALSE)
+      omega <- coef(fit)
+      cpi$omega_matrix[i,] = t(omega[2:nrow(omega)])
+    } # end of protein num for i.
+    ## estimate sigma based on the omega metrix.
+    ## if each row corresponds to one protein, then
+    ## only use features not zero
+    sigma <- mean(colVars(cpi$omega_matrix))
+    cpi$sigma <- sigma
+    for(i in 1:length(cpi$y)){
+      cpi$X[[i]] = t(cpi$X[[i]])
+    }
+    ## run ml-2dqsar with personal code and sigma
+    testResult <- crossPreComPiHier(cpi,
+                                    nfold=nfold,
+                                    sigma2j = sigma,
+                                    sigma2dot = sigma2dot,
+                                    maxit = TRUE,
+                                    iteration = iternum,
+                                    parallel = FALSE)
+    save(cpi, file=paste('cpi_',subdirs[k],'_',features[f],'.data',sep=""))
+    save(testResult, file=paste('testResult_',subdirs[k],'_',features[f],'.data',sep=""))
+  }
+}
--- ml-2dqsar/wiperdata.R
+++ ml-2dqsar/wiperdata.R
@@ -2,22 +2,6 @@
 ### szu
 ### 161023
 
-bifeakeep <- function(datalist,cutoff){
-  ## for binary feature only in datalist$X
-  ##   each column of X is feature.
-  ## return as vector for index to keep.
-  feanum <- ncol(datalist$X[[1]])
-  samplenum <- 0
-  bifeaneed <- 1:feanum
-  colsmeans <- rep(0,feanum)
-  for(prot in 1:length(datalist$X)){
-    colsmeans = colsmeans + colSums(datalist$X[[prot]])
-    samplenum <- samplenum + nrow(datalist$X[[prot]])
-  }
-  colsmeans <- colsmeans/samplenum
-  return(bifeaneed[which( (colsmeans > cutoff) & (colsmeans < (1-cutoff))) ])
-}
-
 loadfiles <- function(datadir,gpcr_nm,kinase_nm,isGPCR,f,k){
   if(isGPCR){
     subdirs <- gpcr_names[subd_index]
@@ -40,4 +24,79 @@ loadfiles <- function(datadir,gpcr_nm,kinase_nm,isGPCR,f,k){
   loadfiles = data.frame(allfiles = files,
                          protnms = dirs)
   return(loadfiles)
+}
+
+getpredata <- function(filesl,){
+  files <- filesl$allfiles
+  dirs <- filesl$protnums
+  oneGroup = list()
+  len = list()
+  oneGroup_xy = list()
+  for (i in seq(files)){
+    oneGroup[[i]] <- fread(as.character(files[i][[1]]), sep=',',header=TRUE)
+    len[i] <- length(oneGroup[[i]]$aveaffinty)
+    ## load data, take -log10 for y.
+    oneGroup[[i]]$aveaffinty <- -log10(oneGroup[[i]]$aveaffinty)
+
+    oneGroup_xy$X[[i]] <- subset(oneGroup[[i]], select = c(-V1,-aveaffinty))
+    ## oneGroup_xy$X[[i]] <- scale(oneGroup_xy$X[[i]], center = TRUE, scale = FALSE)
+    ## oneGroup_xy$xcolm[[i]] <- attributes(oneGroup_xy$X[[i]])$`scaled:center`
+
+    oneGroup_xy$y[[i]] <- subset(oneGroup[[i]], select = aveaffinty)
+    ## oneGroup_xy$y[[i]] <- scale(oneGroup_xy$y[[i]],center = TRUE, scale = FALSE)
+    ## oneGroup_xy$ym[[i]] <- attributes(oneGroup_xy$y[[i]])x$`scaled:center`
+  }
+  return(oneGroup_xy)
+}
+
+bifeakeep <- function(datalist,cutoff){
+  ## for binary feature only in datalist$X
+  ##   each column of X is feature.
+  ## return as vector for index to keep.
+  feanum <- ncol(datalist$X[[1]])
+  samplenum <- 0
+  bifeaneed <- 1:feanum
+  colsmeans <- rep(0,feanum)
+  for(prot in seq(datalist$X)){
+    colsmeans = colsmeans + colSums(datalist$X[[prot]])
+    samplenum <- samplenum + nrow(datalist$X[[prot]])
+  }
+  colsmeans <- colsmeans/samplenum
+  return(bifeaneed[which( (colsmeans > cutoff) & (colsmeans < (1-cutoff))) ])
+}
+
+getcpid <- function(oneGroup_xy, isbinaryf, cutoff, lower_limit, upper_limit){
+  if(isbinaryf){
+    keepft <- bifeakeep(oneGroup_xy,cutoff)
+
+  } else{
+    keepft <- seq(ncol(oneGroup_xy$X[[1]]))
+  }
+
+  for(i in seq(filse)){
+    oneGroup_xy$X[[i]] <- scale(oneGroup_xy$X[[i]], center = TRUE, scale = FALSE)
+    oneGroup_xy$xcolm[[i]] <- attributes(oneGroup_xy$X[[i]])$`scaled:center`
+
+    oneGroup_xy$y[[i]] <- scale(oneGroup_xy$y[[i]],center = TRUE, scale = FALSE)
+    oneGroup_xy$ym[[i]] <- attributes(oneGroup_xy$y[[i]])$`scaled:center`
+  }
+
+  cpi <- list()
+  cpi$proteins <- list()
+  cpi$rootMSE  <- c()
+  j = 0
+  for(i in seq(oneGroup_xy$y)){
+    if(len[[i]]>=lower_limit & len[[i]] <= upper_limit){
+      j = j + 1
+      ## as.character is needed since dirs is factor. f*ck!!!
+      cpi$proteins[j] = as.character(dirs[[i]])
+      cpi$y[[j]] <- oneGroup_xy$y[[i]] # log of affinity
+      cpi$ym[[j]] <- oneGroup_xy$ym[[i]]
+      ## after scale, not data.table. f*ck!!!
+      cpi$X[[j]] <- t(oneGroup_xy$X[[i]][ ,keepft]) # Matrix of the predictors.
+      cpi$xcolm[[j]] <- oneGroup_xy$xcolm[[i]]
+    }
+  }
+  try(if(j==0) stop('No data between "lower_limit" and "upper_limit".'))
+  return(cpi)
 }
